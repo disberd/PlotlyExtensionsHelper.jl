ce0ec4e2abae797eb0a7192fb3d4826192233b4c
Alberto Mengali <disberd@gmail.com>
2024-02-18 15:47:40 +0100

    update CI and tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,13 @@ using SafeTestsets
 
 @safetestset "Extensions loading" begin
     using PlotlyExtensionsHelper
-    using PlotlyBase
     using Test
 
+    @test_throws "No Plotly package has been loaded, make sure to load either PlotlyBase or PlutoPlotly" plotly_plot(rand(5))
+
+    using PlotlyBase
     @test plotly_plot(rand(5)) isa PlotlyBase.Plot
 
     using PlutoPlotly
-
     @test plotly_plot(rand(5)) isa PlutoPlot
 end
