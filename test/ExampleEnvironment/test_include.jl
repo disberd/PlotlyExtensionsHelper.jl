971d4a40d7131b7dca12f611d47cd00b639d3858
Alberto Mengali <disberd@gmail.com>
2024-06-13 12:43:33 +0200

    update some comments

--- test/ExampleEnvironment/test_include.jl
+++ test/ExampleEnvironment/test_include.jl
@@ -5,7 +5,7 @@ try
     Pkg.activate(@__DIR__)
     import PlotlyJS # This will trigger loading the extension
     using ExamplePackage
-    # This will be plotted using PlotlyJS.SyncPlot
+    # This will be plotted using PlotlyJS.SyncPlot (rather than PlotlyBase.Plot) as PlotlyJS is loaded and has a higher priority
     plot_4_lines()
 finally
     Pkg.activate(current_proj)
--- test/ExampleEnvironment/test_notebook.jl
+++ test/ExampleEnvironment/test_notebook.jl
@@ -17,7 +17,7 @@ begin
 end
 
 # ╔═╡ 4e134046-ff35-417f-864f-96ceb93c22e9
-# This will be plotted with PlutoPlotly as it's the highest priority
+# This will be plotted with PlutoPlotly (rather than PlotlyBase) as it's the highest priority
 plot_4_lines()
 
 # ╔═╡ Cell order:
